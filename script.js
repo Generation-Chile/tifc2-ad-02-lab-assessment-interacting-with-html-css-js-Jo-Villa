e68aac5a6c207227fc4ac70964c187a8c892534e
Jo-Villa <josefina.villalobos@genstudents.org>
2025-05-01 00:48:13 +0000

    tarea terminada

---
+++ script.js
@@ -0,0 +1,10 @@
+// Cambiar el primer "¡Hola mundo!" a "Adiós"
+document.getElementById("holamundo").innerText = "Adiós";
+
+// Cambiar el color de fuente del primer encabezado a naranja
+document.querySelector("h1").style.color = "orange";
+
+function Cambiocolor() {
+  // Selecciona el elemento con id "cambiomarron" y cambia su color a marrón
+  document.getElementById("cambiomarron").style.color = "brown";
+}
